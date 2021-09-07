d406ac6557176c7aec687d5f9eed0064103c7b8e
lesonion <blomgren.andin@hotmail.com>
2021-09-07 14:05:14 +0200

    Remade structure for Lab2 and project overall

---
+++ Lab2/src/RatNum.java
@@ -0,0 +1,12 @@
+public class RatNum {
+
+    //För att kunna göra denna förkortning behövs en klassmetod  som beräknar den största gemensamma divisorn till två heltal m  och n
+    public static int gcd(int m, int n){
+
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
